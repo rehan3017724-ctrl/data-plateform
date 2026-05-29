7c5c66fff86504585ddf7f5fe24abf6919fd1f82
rehan3017724-ctrl <rehan3017724@gmail.com>
2026-05-30 02:13:01 +0530

    Update sales_ingestion.py

---
+++ pyspark/sales_ingestion.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("sales_ingestion").getOrCreate()
+
+df = spark.read.option("header", "true").csv(
+    "gs://demo-bucket/raw/sales.csv"
+)
+
+df.write.mode("overwrite").format("bigquery") \
+    .option("table", "project.bronze.sales") \
+    .save()
+
+spark.stop()
